f96979eeefd30a687405bc200566cbc634879d56
Leonardo Lucena <leonardolucena.cc@gmail.com>
2023-11-30 08:49:35 -0300

    :construction: feat: Add feature for user remove

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
-  const UserTile(this.user, {super.key});
+  const UserTile(this.user, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +22,37 @@ class UserTile extends StatelessWidget {
         child: Row(
           children: <Widget>[
             IconButton(
-                icon: const Icon(Icons.edit),
-                color: Colors.orangeAccent,
-                onPressed: () {}),
+              icon: const Icon(Icons.edit),
+              color: Colors.orangeAccent,
+              onPressed: () {},
+            ),
             IconButton(
-                icon: const Icon(Icons.delete),
-                color: Colors.red.shade500,
-                onPressed: () {}),
+              icon: const Icon(Icons.delete),
+              color: Colors.red.shade500,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text('Excluir Usuário'),
+                    content: const Text('Tem certeza?'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(false),
+                        child: const Text('Não'),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(true),
+                        child: const Text('Sim'),
+                      ),
+                    ],
+                  ),
+                ).then((confirmed) {
+                  if (confirmed) {
+                    Provider.of<Users>(context, listen: false).remove(user);
+                  }
+                });
+              },
+            ),
           ],
         ),
       ),
